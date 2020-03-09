bd21bd53a334a832ff57cfd92c241391c4a00b29
Sannisuvi <sanni.kytola@hotmail.com>
2020-03-09 15:11:48 +0200

    Start sidebar component.

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const SideBar = (props) => (
   <div id={'sideBar'}>
-
+    <p>Recently Viewed</p>
+    <ul>
+      <li>Post you viewed</li>
+      <li>Post you viewed before that</li>
+      <li>Post you viewed first</li>
+    </ul>
+    <p>Archive</p>
+    <ul>
+      <li>2020</li>
+      <li>2019</li>
+    </ul>
   </div>
 )
