4002020dae7796aea227b3e9307f650956800142
Heikki Kangas <heikki.kangas@tuni.fi>
2020-03-09 11:55:22 +0200

    Start react project.

---
+++ src/Banner.js
@@ -0,0 +1,2 @@
+import React from 'react'
+export const Banner = (props) => <div id={'banner'}><h1>{props.title}</h1></div>
---
+++ src/TopBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const TopBar = (props) => {
+  return (
+    <div id={'topBar'}>
+      <button>Button 1</button>
+      <button>Button 2</button>
+      <button>Button 3</button>
+      <button>Button 4</button>
+    </div>
+  )
+}
