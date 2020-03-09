d76eeabc2cf179abbd2cb1a712c4b07ff40f396b
kanerla <kanervalaurajohanna@gmail.com>
2020-03-09 17:44:24 +0200

    Create CreatePost component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Banner } from './Banner'
 import { TopBar } from './TopBar'
 import { SideBar } from './SideBar'
 import { Posts } from './Posts'
+import { CreatePost } from './CreatePost'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 export const App = () => {
@@ -14,6 +15,7 @@ export const App = () => {
         <TopBar />
         <SideBar />
         <Route exact={true} path={'/'} component={Posts}/>
+        <Route path={'/createpost'} component={CreatePost}/>
       </div>
     </BrowserRouter>
   )
