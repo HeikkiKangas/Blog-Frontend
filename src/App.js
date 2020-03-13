15f0fbe6eba613c4ae6e1c895a8d8295c0d18e44
Heikki Kangas <heikki.kangas@tuni.fi>
2020-03-13 14:01:24 +0200

    Move Snackbar to App.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import { TopBar } from './TopBar'
 import { Posts } from './Posts'
 import { CreatePost } from './CreatePost'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Slide, Snackbar } from '@material-ui/core'
 
 export const App = () => {
+  const [snackbarState, setSnackbarState] = React.useState({ open: false, text: '' })
+
   return (
     <BrowserRouter>
       <div>
         <Banner title={'Pöhinää ja pastellivärejä'}/>
         <TopBar />
-        <Route exact={true} path={'/'} component={Posts}/>
-        <Route path={'/createpost'} component={CreatePost}/>
+        <Route
+          exact={true}
+          path={'/'}
+          render={() => <Posts setSnackbarState={setSnackbarState}/>}/>
+
+        <Route
+          path={'/createpost'}
+          component={CreatePost}/>
+
+        <Snackbar
+          open={snackbarState.open}
+          onClose={() => setSnackbarState({ ...snackbarState, open: false })}
+          TransitionComponent={Slide}
+          message={snackbarState.text}
+          autoHideDuration={2000}/>
       </div>
     </BrowserRouter>
   )
