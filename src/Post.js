7a04325c81613a2b66e9f0e3e92f53fa339597d8
Heikki Kangas <heikki.kangas@tuni.fi>
2020-03-15 22:56:15 +0200

    Move Post from Posts to separate file.

---
+++ src/Post.js
@@ -0,0 +1,92 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Button, Dialog, DialogActions, DialogTitle, Icon, IconButton, Tooltip } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+export const Post = (props) => {
+  const { post } = props
+  return (
+    <div className='post'>
+      <EditButton id={post.id}/>
+      <DeleteButton {...props} />
+      <h1 className='title'>{post.title}</h1>
+      <h3 className='author'>{post.author.userName}</h3>
+      {post.author.admin ? <p className='authorIsAdmin'>Admin user</p> : null}
+      <p className='timestamp'>{post.timestamp}</p>
+      <p className='text'>{post.text}</p>
+      <Comments count={post.comments.length}/>
+      <Likes count={post.likes.length}/>
+      <Tags tags={post.tags}/>
+    </div>
+  )
+}
+
+const Comments = ({ count }) => <a id='commentCount' href='localhost:3000'>{count} Comments</a>
+const Likes = ({ count }) => <a id='likeCount' href='localhost:3000'>{count} Likes</a>
+const Tags = ({ tags }) => <div id='tags'>{tags.map(tag => <a href='localhost:3000' key={tag}>{tag}</a>)}</div>
+
+const DeleteButton = (props) => {
+  const [open, setOpen] = React.useState(false)
+  const handleCloseDialog = () => setOpen(false)
+
+  const deletePost = async ({ post, posts, setPosts, setSnackbarState }) => {
+    console.log('Deleting post id:' + post.id)
+    const response = await fetch('http://localhost:8080/api/posts/' + post.id, { method: 'delete' })
+      .catch(console.log)
+
+    const newState = { open: true, text: `Could not delete ${post.title}.` }
+    if (response.ok) {
+      setPosts(posts.filter(p => p.id !== post.id))
+      newState.text = `${post.title} deleted.`
+    }
+    setSnackbarState(newState)
+  }
+
+  console.log(props)
+  return (
+    <>
+      <Button
+        className='deleteButton'
+        variant='contained'
+        fullWidth={false}
+        color='secondary'
+        startIcon={<Icon>delete</Icon>}
+        onClick={() => setOpen(true)}/>
+      <Tooltip title='Delete'>
+        <IconButton
+          className='deleteIcon'
+          color='primary'>
+          <Icon>delete</Icon>
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='Edit post'>
+        <IconButton
+          className='editIcon'
+          color='primary'>
+          <Icon>edit</Icon>
+        </IconButton>
+      </Tooltip>
+      <Dialog open={open} onClose={handleCloseDialog}>
+        <DialogTitle id='delete-dialog-title'>{`Delete post "${props.post.title}"?`}</DialogTitle>
+        <DialogActions>
+          <Button class variant='contained' color='secondary' onClick={handleCloseDialog}>
+                        Cancel
+          </Button>
+          <Button variant='contained' color='secondary' onClick={() => deletePost(props)}>
+                        Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
+const EditButton = (props) => (
+  <Button
+    id={'editButton'}
+    variant={'contained'}
+    fullWidth={false}
+    color={'secondary'}
+    startIcon={<Icon>edit</Icon>}
+    component={Link} to={'/editpost/' + props.id}/>
+)
--- src/Posts.js
+++ src/Posts.js
@@ -3,10 +3,11 @@ import React from 'react'
 import './posts.css'
 import { SideBar } from './SideBar'
 import { Link } from 'react-router-dom'
-import { Button, Dialog, DialogActions, DialogTitle} from '@material-ui/core'
+import { Button } from '@material-ui/core'
+import { Post } from './Post'
 
 export const Posts = ({ snackbarState, setSnackbarState }) => {
-  const [posts, setPosts] = React.useState(null)
+  const [posts, setPosts] = React.useState([])
 
   const fetchPosts = () => fetch('http://localhost:8080/api/posts')
     .then(response => response.json())
@@ -23,7 +24,7 @@ export const Posts = ({ snackbarState, setSnackbarState }) => {
 
   return (
     <div>
-      <div id='generatePostsButton'>
+      <div id='generatePostsButtonDiv'>
         <Button
           id='generatePostsButton'
           variant='contained'
@@ -48,81 +49,5 @@ export const Posts = ({ snackbarState, setSnackbarState }) => {
       </div>
       <SideBar />
     </div>
-  )
-}
-
-const Post = (props) => {
-  const { post } = props
-  return (
-    <div className='post'>
-      <EditButton id={post.id}/>
-      <DeleteButton {...props} />
-      <h1 className='title'>{post.title}</h1>
-      <h3 className='author'>{post.author.userName}</h3>
-      {post.author.admin ? <p className='authorIsAdmin'>Admin user</p> : null}
-      <p className='timestamp'>{post.timestamp}</p>
-      <p className='text'>{post.text}</p>
-      <Comments count={post.comments.length}/>
-      <Likes count={post.likes.length}/>
-      <Tags tags={post.tags}/>
-    </div>
-  )
-}
-
-const Comments = ({ count }) => <a id='commentCount' href='localhost:3000'>{count} Comments</a>
-const Likes = ({ count }) => <a id='likeCount' href='localhost:3000'>{count} Likes</a>
-const Tags = ({ tags }) => <div id='tags'>{tags.map(tag => <a href='localhost:3000' key={tag}>{tag}</a>)}</div>
-
-const EditButton = (props) => (
-  <Button
-    id={'editButton'}
-    variant={'contained'}
-    fullWidth={false}
-    color={'secondary'}
-    component={Link} to={'/editpost/' + props.id}>
-    Edit
-  </Button>
-)
-
-const DeleteButton = (props) => {
-  const [open, setOpen] = React.useState(false)
-  const handleCloseDialog = () => setOpen(false)
-
-  const deletePost = async ({ post, posts, setPosts, setSnackbarState }) => {
-    console.log('Deleting post id:' + post.id)
-    const response = await fetch('http://localhost:8080/api/posts/' + post.id, { method: 'delete' })
-      .catch(console.log)
-
-    const newState = { open: true, text: `Could not delete ${post.title}.` }
-    if (response.ok) {
-      setPosts(posts.filter(p => p.id !== post.id))
-      newState.text = `${post.title} deleted.`
-    }
-    setSnackbarState(newState)
-  }
-
-  console.log(props)
-  return (
-    <>
-      <Button
-        id={'deleteButton'}
-        variant={'contained'}
-        fullWidth={false}
-        color={'secondary'}
-        onClick={() => setOpen(true)}>
-            Delete
-      </Button>
-      <Dialog open={open} onClose={handleCloseDialog}>
-        <DialogTitle id='delete-dialog-title'>{`Delete post "${props.post.title}"?`}</DialogTitle>
-        <DialogActions>
-          <Button variant='contained' color='secondary' onClick={handleCloseDialog}>
-            Cancel
-          </Button>
-          <Button variant='contained' color='secondary' onClick={() => deletePost(props)}>
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
   )
 }
