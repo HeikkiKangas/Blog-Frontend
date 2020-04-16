2f402271f7061ac56c7971fb4c8693b70a73cf5a
Heikki Kangas <heikki.kangas@tuni.fi>
2020-04-16 21:43:43 +0300

    Full width SearchBar.

--- src/TopBar.js
+++ src/TopBar.js
@@ -9,46 +9,26 @@ export const TopBar = (props) => {
   return (
     <div id={'topBar'}>
       <CreatePostButton/>
-      <SearchBar/>
       <LoginButton loggedIn={false}/>
+      <SearchBar/>
     </div>
   )
 }
 
 const SearchBar = ({ posts }) => {
+  console.log('TopBar')
   console.log(posts)
-  /*
-  const handleChange = (e) => {
-    console.log(e.target.value)
-    const object = document.getElementById('post')
-    console.log(object.textContent)
 
-    const searchBarText = e.target.value
-    const divText = object.textContent
-    if (divText.includes(searchBarText)) {
-      console.log('the div text contains your search text')
-    } else {
-      console.log("the div text doesn't contain search text")
-    }
-  }
-  */
   return (
     <Autocomplete
-      style={{ width: 300 }}
-      id="searchbar"
-      freeSolo
-      options={posts !== undefined ? posts.map(p => p.title) : []}
+      id="searchBar"
+      options={posts === undefined ? [] : posts.map(p => p.title)}
       autoComplete={true}
       openOnFocus={false}
       renderInput={(params) => (
         <TextField {...params} label="Search posts" margin="normal" variant="outlined" />
       )}/>
   )
-  /*
-    return (
-      <input type="text" id={'searchBar'} className="input" onChange={handleChange} placeholder="Search..." />
-    )
-  */
 }
 
 const CreatePostButton = (props) => (
