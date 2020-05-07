90b0cec424b67e67acc1db7f2a2d751f8ea161bf
Heikki Kangas <heikki.kangas@tuni.fi>
2020-05-07 04:32:54 +0300

    Update Banner url.

--- src/Banner.js
+++ src/Banner.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import './banner.css'
 
-export const Banner = (props) => <div id={'banner'}><h1><a id="bannerLink" href="http://localhost:3000/" >{props.title}</a></h1></div>
+export const Banner = (props) => <div id={'banner'}><h1><a id="bannerLink" href="/" >{props.title}</a></h1></div>
