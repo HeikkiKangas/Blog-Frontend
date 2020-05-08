fc59406242398fd57f73d18e61c34430f921dd6e
Heikki Kangas <heikki.kangas@tuni.fi>
2020-05-08 17:38:17 +0300

    Fix setUser.

--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import './Login.css'
@@ -12,16 +13,18 @@ export const Login = (props) => {
     return username.length > 0 && password.length > 0
   }
 
-  const handleSubmit = async event => {
+  const handleSubmit = event => {
     event.preventDefault()
 
-    const response = await fetch(`${API_URL}/users/login`, {
+    fetch(`${API_URL}/users/login`, {
       headers: new Headers({
         Authorization: `Basic ${btoa(`${username}:${password}`)}`,
         username: username
       })
     })
-    if (response.ok) props.setUser({ username: username, password: password, admin: response.json().admin })
+      .then(response => response.json())
+      .then(json => props.setUser({ username: username, password: password, admin: json.admin }))
+      .catch(console.log)
   }
 
   return (
